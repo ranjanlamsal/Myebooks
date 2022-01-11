5f87b9b6ae8a6631a0aec9345cb0c94f6dcd601e
Ranjan <lamsalranjana19@gmail.com>
2022-01-11 18:04:43 +0545

    created first app of my commercial website and migrated the changes

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "ShopHome"),
+    path('about/', views.about, name = "AboutUs"),
+    path('contact/', views.contact, name = "ContactUS"),
+    path('search/', views.search, name = "Search"),
+    path('productview/', views.productview, name = "ProductView"),
+    path('checkout/', views.checkout, name = "Checkout"),
+    path('tracker/', views.tracker, name = "TrackingStatus"),
 ]
